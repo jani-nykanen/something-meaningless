cd91af7ea39f16367cc5c0768435a0c921cc55d0
jani-nykanen <jnyknn@gmail.com>
2022-03-24 09:49:47 +0200

    Pause menu

--- src/game/game.ts
+++ src/game/game.ts
@@ -3,8 +3,16 @@ import { CoreEvent, Scene } from "../core/core.js";
 import { TransitionEffectType } from "../core/transition.js";
 import { State } from "../core/types.js";
 import { RGBA } from "../core/vector.js";
+import { Menu, MenuButton } from "./menu.js";
 import { Stage } from "./stage.js";
 
+
+const HINTS = [
+    "HINT: Use arrow keys to move.",
+    "HINT: You can jump over small gaps.",
+    "HINT: Press Backspace or Z\nto undo a move.",
+    "HINT: You can restart the\nstage by pressing R."
+];
 
 
 export class GameScene implements Scene {
@@ -15,10 +23,42 @@ export class GameScene implements Scene {
     private scaleOutFactor : number;
     private fadingOut : boolean;
 
+    private pauseMenu : Menu;
+
 
     constructor(param : any, event : CoreEvent) {
 
         this.stage = new Stage(event, 1);
+
+        this.pauseMenu = new Menu(
+        [
+            new MenuButton("Resume", event => {
+
+                this.pauseMenu.deactivate();
+            }),
+
+            new MenuButton("Restart", event => {
+
+                this.pauseMenu.deactivate();
+                this.stage.reset();
+            }),
+
+            new MenuButton("Undo", event => {
+
+                this.pauseMenu.deactivate();
+                this.stage.undo();
+            }),
+
+            new MenuButton("Settings", event => {
+
+                // ...
+            }),
+
+            new MenuButton("Main menu", event => {
+
+                // ...
+            })
+        ]);
     }
 
 
@@ -38,6 +78,17 @@ export class GameScene implements Scene {
             return;
         }
         this.scaleOutFactor = 0.0;
+
+        if (this.pauseMenu.isActive()) {
+            
+            this.pauseMenu.update(event);
+            return;
+        }
+        else if (event.input.getAction("start") == State.Pressed) {
+
+            this.pauseMenu.activate(0);
+        }
+
 
         this.stage.update(event);
 
@@ -67,6 +118,8 @@ export class GameScene implements Scene {
 
     private drawHUD(canvas : Canvas) {
 
+        const HINT_OFFSET = [0, 0, -52, -52];
+
         canvas.changeShader(ShaderType.Textured);
 
         canvas.transform
@@ -80,6 +133,27 @@ export class GameScene implements Scene {
         canvas.setColor();
         canvas.drawText(canvas.assets.getBitmap("font"), 
             str, view.x/2, 16, -56, 0, TextAlign.Center, 0.67, 0.67);
+
+        let index = this.stage.getIndex() - 1;
+        if (index < HINTS.length) {
+
+            canvas.drawText(canvas.assets.getBitmap("font"), 
+                HINTS[index], 16, view.y-64 + HINT_OFFSET[index], 
+                -56, -4, TextAlign.Left, 0.40, 0.40);
+        }
+    }
+
+
+    private drawPauseMenu(canvas : Canvas) {
+
+        let view = canvas.transform.getViewport();
+
+        canvas.changeShader(ShaderType.NoTexture);
+        canvas.setColor(0, 0, 0, 0.67);
+        canvas.fillRect(0, 0, view.x, view.y);
+
+        canvas.changeShader(ShaderType.Textured);
+        this.pauseMenu.draw(canvas, 0, 0, -56, 72, 0.50, Math.PI*2 / 6, 8.0);
     }
 
 
@@ -108,6 +182,11 @@ export class GameScene implements Scene {
         this.stage.draw(canvas, scaleOut, yoff);
 
         this.drawHUD(canvas);
+
+        if (this.pauseMenu.isActive()) {
+
+            this.drawPauseMenu(canvas);
+        }
     }
 
     
---
+++ src/game/menu.ts
@@ -0,0 +1,169 @@
+import { Canvas, TextAlign } from "../core/canvas.js";
+import { CoreEvent } from "../core/core.js";
+import { negMod } from "../core/math.js";
+import { State } from "../core/types.js";
+
+
+
+export class MenuButton {
+
+
+    private text : string;
+    private callback : (event : CoreEvent) => void;
+
+
+    constructor(text : string, callback : (event : CoreEvent) => void) {
+
+        this.text = text;
+        this.callback = callback;
+    }
+
+
+    public getText = () : string => this.text;
+    public evaluateCallback = (event : CoreEvent) => this.callback(event);
+
+
+    public clone() : MenuButton {
+
+        return new MenuButton(this.text, this.callback);
+    }
+
+
+    public changeText(newText : string) {
+
+        this.text = newText;
+    }
+}
+
+
+export class Menu {
+
+
+    private buttons : Array<MenuButton>;
+
+    private cursorPos : number;
+    private active : boolean;
+
+    private textWave : number;
+
+
+    constructor(buttons : Array<MenuButton>) {
+
+        this.buttons = (new Array<MenuButton> (buttons.length))
+            .fill(null)
+            .map((_, i) => buttons[i].clone());
+
+        // this.maxLength = Math.max(
+        //    ...this.buttons.map(b => b.getText().length));
+
+        this.cursorPos = 0;
+        this.active = false;
+
+        this.textWave = 0.0;
+    }
+
+
+    public activate(cursorPos = -1) {
+
+        if (cursorPos >= 0)
+            this.cursorPos = cursorPos % this.buttons.length;
+
+        this.active = true;
+    }
+
+
+    public update(event : CoreEvent) {
+
+        const WAVE_SPEED = 0.067;
+
+        if (!this.active) return;
+
+        let oldPos = this.cursorPos;
+
+        if (event.input.upPress()) {
+
+            -- this.cursorPos;
+        }
+        else if (event.input.downPress()) {
+
+            ++ this.cursorPos;
+        }
+
+        if (oldPos != this.cursorPos) {
+
+            this.cursorPos = negMod(this.cursorPos, this.buttons.length);
+
+            event.audio.playSample(event.assets.getSample("choose"), 0.70);
+        }
+
+        let activeButton = this.buttons[this.cursorPos];
+        
+        if (event.input.getAction("select") == State.Pressed ||
+            event.input.getAction("start") == State.Pressed) {
+
+            event.audio.playSample(event.assets.getSample("select"), 0.50);    
+            activeButton.evaluateCallback(event);
+        }
+
+        this.textWave = (this.textWave + WAVE_SPEED*event.step) % (Math.PI*2);
+    }
+
+
+    public draw(canvas : Canvas, x : number, y : number,
+        xoff = -56, yoff = 64, fontScale = 0.67, 
+        wavePeriod = 0, waveAmplitude = 0) {
+
+        const ACTIVE_SCALE = 1.25;
+
+        if (!this.active) return;
+
+        let font = canvas.assets.getBitmap("font");
+
+        let view = canvas.transform.getViewport();
+
+        let h = (this.buttons.length * yoff);
+        let dx = view.x/2 + x;
+        let dy = view.y/2 - h / 2 + y;
+
+        let scale : number;
+
+        let waveFactor : number;
+
+        for (let i = 0; i < this.buttons.length; ++ i) {
+
+            if (i == this.cursorPos) {
+
+                waveFactor = 1.0;
+                canvas.setColor(1, 1, 0.33);
+                scale = fontScale * ACTIVE_SCALE;
+            }
+            else {
+
+                waveFactor = 0.0;
+                canvas.setColor();
+                scale = fontScale;
+            }
+
+            canvas.drawText(font, this.buttons[i].getText(), 
+                dx, dy + i * yoff, xoff, 0, TextAlign.Center,
+                scale, scale, this.textWave, 
+                waveFactor * waveAmplitude, wavePeriod);
+        } 
+        canvas.setColor();
+    }
+
+
+    public isActive = () : boolean => this.active;
+
+
+    public deactivate() {
+
+        this.active = false;
+    }
+
+
+    public changeButtonText(index : number, text : string) {
+
+        this.buttons[index].changeText(text);
+    }
+}
